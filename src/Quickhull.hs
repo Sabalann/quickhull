4dfbb891ea618492f32640e1cf0fe47fb7817070
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2025-01-23 11:02:29 +0100

    initialPartition passed the tests, made some changes to partition

--- src/Quickhull.hs
+++ src/Quickhull.hs
@@ -59,59 +59,73 @@ type SegmentedPoints = (Vector Bool, Vector Point)
 initialPartition :: Acc (Vector Point) -> Acc SegmentedPoints
 initialPartition points =
   let
+      -- Find leftmost and rightmost points
       p1, p2 :: Exp Point
-      p1 = the $ fold1 (\a b -> fst a < fst b ? (a, b)) points
-      p2 = the $ fold1 (\a b -> fst a > fst b ? (a, b)) points
+      p1 = the $ fold1 (\a b -> 
+          fst a < fst b ? (a, 
+          fst a == fst b ? (snd a <= snd b ? (a, b), b)
+          )) points
+      p2 = the $ fold1 (\a b -> 
+          fst a > fst b ? (a, 
+          fst a == fst b ? (snd a >= snd b ? (a, b), b)
+          )) points
 
+      -- Carefully separate points above and below the line
+      -- Note: points exactly on the line are excluded
       isUpper :: Acc (Vector Bool)
-      isUpper = map (\p -> p /= p1 && p /= p2 && pointIsLeftOfLine (T2 p1 p2) p) points
+      isUpper = map (\p -> 
+          p /= p1 && p /= p2 && 
+          pointIsLeftOfLine (T2 p1 p2) p
+        ) points
 
       isLower :: Acc (Vector Bool)
-      isLower = map (\p -> p /= p1 && p /= p2 && pointIsLeftOfLine (T2 p2 p1) p) points
+      isLower = map (\p -> 
+          p /= p1 && p /= p2 && 
+          pointIsLeftOfLine (T2 p2 p1) p
+        ) points
 
-      offsetUpper :: Acc (Vector Int)
-      countUpper  :: Acc (Scalar Int)
-      T2 offsetUpper countUpper = 
-        let boolToInt = map (\b -> b ? (1, 0)) isUpper
-            offsets = prescanl (+) 0 boolToInt
-            count = fold (+) 0 boolToInt
-        in T2 offsets count
+      -- Count points in upper and lower segments
+      upperCount :: Acc (Scalar Int)
+      upperCount = fold (+) 0 (map (\b -> b ? (1, 0)) isUpper)
 
-      offsetLower :: Acc (Vector Int)
-      countLower  :: Acc (Scalar Int)
-      T2 offsetLower countLower = 
-        let boolToInt = map (\b -> b ? (1, 0)) isLower
-            offsets = prescanl (+) 0 boolToInt
-            count = fold (+) 0 boolToInt
-        in T2 offsets count
+      lowerCount :: Acc (Scalar Int)
+      lowerCount = fold (+) 0 (map (\b -> b ? (1, 0)) isLower)
 
-      destination :: Acc (Vector (Maybe DIM1))
-      destination = generate (shape points) $ \ix ->
-        let i = unindex1 ix
-        in isUpper ! ix ? (Just_ (index1 (1 + offsetUpper ! ix)),
-          isLower ! ix ? (Just_ (index1 (2 + the countUpper + offsetLower ! ix)),
-          Nothing_))
+      -- Total output size: p1 + upper points + p2 + lower points + p1
+      totalSize = 3 + the upperCount + the lowerCount
 
+      -- Compute destination indices for points
+      destinations :: Acc (Vector Int)
+      destinations = generate (shape points) $ \ix ->
+        let 
+            i = unindex1 ix
+            point = points ! ix
+            upperOffset = prescanl (+) 0 (map (\b -> b ? (1, 0)) isUpper)
+            lowerOffset = prescanl (+) 0 (map (\b -> b ? (1, 0)) isLower)
+        in
+        point == p1 ? (0,                             -- First p1 goes first
+            point == p2 ? (1 + the upperCount,        -- p2 goes after upper points
+            isUpper ! ix ? (1 + upperOffset ! ix,     -- Upper points after p1 
+            isLower ! ix ? (2 + the upperCount + lowerOffset ! ix, -1)  -- Lower points after p2
+            )))
+
+      -- Create points array with careful positioning
       newPoints :: Acc (Vector Point)
       newPoints = permute 
         const
-        (fill (index1 (3 + the countUpper + the countLower)) p1) -- Initialize with p1
-        (\ix -> let i = unindex1 ix
-          in i == 1 + the countUpper  -- p2 position
-             ? (Just_ (index1 1),      -- index of p2 in input
-             destination ! ix))        -- other points
+        (fill (index1 totalSize) p1)  -- Initialize all with p1
+        (\ix -> destinations ! ix /= -1 
+            ? (Just_ (index1 (destinations ! ix)), 
+               Nothing_))
         points
 
+      -- Create head flags marking segment boundaries
       headFlags :: Acc (Vector Bool)
-      headFlags = permute 
-        (||)
-        (fill (index1 (3 + the countUpper + the countLower)) (constant False))
-        (\ix -> let i = unindex1 ix
-          in (i == 0 ||                            -- Start (p1)
-              i == (1 + the countUpper) ||         -- Middle (p2)
-              i == (2 + the countUpper + the countLower)) -- End (p1)
-             ? (Just_ ix, Nothing_))
-        (fill (index1 1) (constant True))
+      headFlags = generate (index1 totalSize) $ \(I1 i) ->
+        i == 0 ||                                     -- Start (first p1)
+        i == 1 + the upperCount ||                    -- Middle (p2)
+        i == totalSize - 1                            -- End (last p1)
+
   in
   T2 headFlags newPoints
 
@@ -127,7 +141,66 @@ initialPartition points =
 --
 partition :: Acc SegmentedPoints -> Acc SegmentedPoints
 partition (T2 headFlags points) =
-  error "TODO: partition"
+  let
+      segStarts = propagateL headFlags points
+      segEnds = rotate1 segStarts
+
+      p3s = segmentedScanl1 
+        (\a b -> 
+          let p1 = head' segStarts
+              p2 = last' segStarts
+              d1 = nonNormalizedDistance (T2 p1 p2) a
+              d2 = nonNormalizedDistance (T2 p1 p2) b
+          in d1 > d2 ? (a, b)) 
+        headFlags 
+        points
+      
+      isLeft = zipWith3 
+        (\p1 p3 p -> p /= p1 && p /= p3 && pointIsLeftOfLine (T2 p1 p3) p)
+        segStarts p3s points
+                
+      isRight = zipWith3 
+        (\p3 p2 p -> p /= p3 && p /= p2 && pointIsLeftOfLine (T2 p3 p2) p)
+        p3s segEnds points
+
+      T2 offsetLeft countLeft = 
+        let boolToInt = map (\b -> b ? (1, 0)) isLeft
+        in T2 (prescanl (+) 0 boolToInt) (fold (+) 0 boolToInt)
+
+      T2 offsetRight countRight = 
+        let boolToInt = map (\b -> b ? (1, 0)) isRight
+        in T2 (prescanl (+) 0 boolToInt) (fold (+) 0 boolToInt)
+
+      totalPoints = 3 + the countLeft + the countRight
+
+      newPoints = permute const
+        (fill (index1 totalPoints) (head' segStarts))
+        (\ix -> let i = unindex1 ix
+               in 
+                  i == 0 ? (Just_ (index1 0),
+                  i == 1 ? (Just_ (index1 1),
+                  i == 2 + the countLeft ? (Just_ (index1 (2 + the countLeft)),
+                  i == totalPoints - 1 ? (Just_ (index1 0),  -- Force last point to be first point
+                  isLeft ! ix 
+                  ? (Just_ (index1 (2 + offsetLeft ! ix)),
+                  isRight ! ix 
+                  ? (Just_ (index1 (3 + the countLeft + offsetRight ! ix)),
+                  Nothing_)))))))
+        points
+
+      newHeadFlags = generate 
+        (index1 totalPoints)
+        (\ix -> let i = unindex1 ix
+               in i == 0 || 
+                  i == 1 || 
+                  i == 2 + the countLeft ||
+                  i == totalPoints - 1)
+  in
+  T2 newHeadFlags newPoints
+
+  where 
+    head' xs = the $ fold1 const xs
+    last' xs = the $ fold1 (const P.id) xs
 
 
 -- The completed algorithm repeatedly partitions the points until there are
@@ -140,6 +213,19 @@ quickhull =
 
 -- Helper functions
 -- ----------------
+
+-- Get first element of vector
+indexHead :: Elt a => Acc (Vector a) -> Exp a
+indexHead arr = arr ! index1 0
+
+-- Get last element of vector
+indexLast :: Elt a => Acc (Vector a) -> Exp a
+indexLast arr = arr ! index1 (size arr - 1)
+
+rotate1 :: Elt a => Acc (Vector a) -> Acc (Vector a)
+rotate1 arr = generate (shape arr) (\ix ->
+    let i = unindex1 ix
+    in i == 0 ? (arr ! (index1 (size arr - 1)), arr ! (index1 (i - 1))))
 
 propagateL :: Elt a => Acc (Vector Bool) -> Acc (Vector a) -> Acc (Vector a)
 propagateL flags values =
